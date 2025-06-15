aa0df2dd40e9b29c30ef07912cf75d5650f5e430
HikariNee <aneris@disroot.org>
2025-06-15 16:17:08 +0530

    switch to using std::cerr and std::strerror.

--- src/error_handling.cpp
+++ src/error_handling.cpp
@@ -1,32 +1,35 @@
 #include <sys/mount.h>
 #include <cstdlib>
+#include <cstring>
+#include <iostream>
 #include <errno.h>
+#include <mqueue.h>
 #include "error_handling.hpp"
 #include "config.hpp"
 
 auto cleanup() -> void
 {
   if (umount2((ROOTFS_PATH / "dev").c_str(), MNT_DETACH) == -1) {
-    perror("umount_rootfs_dev");
+    std::cerr << "umount_rootfs_dev" << ": " << std::strerror(errno) << '\n';
   }
 
   if (umount2((ROOTFS_PATH / "proc").c_str(), MNT_DETACH) == -1) {
-    perror("umount_rootfs_proc");
+    std::cerr << "umount_rootfs_proc" << ": " << std::strerror(errno) << '\n';
   }
 
   if (umount2((ROOTFS_PATH / "sys").c_str(), MNT_DETACH) == -1) {
-    perror("umount_rootfs_sys");
+    std::cerr << "umount_rootfs_sys" << ": " << std::strerror(errno) << '\n';
   }
 
   if (umount2((ROOTFS_PATH / "tmp").c_str(), MNT_DETACH) == 1) {
-    perror("umount_rootfs_tmp");
+    std::cerr << "umount_rootfs_tmp" << ": " << std::strerror(errno) << '\n';
   }
 }
 
 
 auto panicOnError(const std::string& msg) -> void
 {
-  perror(msg.c_str());
+  std::cerr << msg << ": " << std::strerror(errno) << std::endl;
   cleanup();
   std::exit(EXIT_FAILURE);
 }
